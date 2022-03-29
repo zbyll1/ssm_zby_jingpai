0807436dd0fc690698a534ebd54fe379f6d4cbae
zby <1831613271@qq.com>
2022-03-29 11:10:05 +0800

    111

---
+++ zby/src/main/java/A.java
@@ -0,0 +1,10 @@
+/**
+ * @author 张滨宇
+ * @version 1.0
+ * @date 2022/3/29 11:09
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
